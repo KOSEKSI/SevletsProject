96805fe8a5d8900ad6cf7095b5e499e4f3772be9
Koseksi Pachipulusula <49813796+KOSEKSI@users.noreply.github.com>
2019-06-29 12:32:31 +0530

    commited packages changes by srikanth

--- src/main/java/com/koseksi/service/SendMailJavaService.java
+++
@@ -1,9 +0,0 @@
-package com.koseksi.service;
-
-import com.koseksi.dto.EmailDetailsDto;
-
-public interface SendMailJavaService {
-
-	
-	public String sendMail(EmailDetailsDto dto) throws Exception;
-}
